1fd3b87528f7b213ce9dd9790b8eb61041ea3693
MustafaRamadan1 <Mustafa.Ramadan165@gmail.com>
2023-06-29 15:53:05 +0300

    PropertyDetails , The basic Structure and some format

--- src/components/property/PropertyDetails.jsx
+++ src/components/property/PropertyDetails.jsx
@@ -1,7 +1,8 @@
 import { useLocation } from 'react-router-dom';
 
 import './propertyDetails.css';
-
+import ApartmentIcon from '@mui/icons-material/Apartment';
+import DescriptionIcon from '@mui/icons-material/Description';
 const PropertyDetails = () => {
 
     const location = useLocation();
@@ -14,23 +15,29 @@ const PropertyDetails = () => {
         //     <p>{property.subscribe}</p>
         // </div>
 
-        <section className="text-gray-600 body-font main-section">
-        <div className="container  mx-auto  px-5 py-24 row items-center">
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0 md:col-8">
-            <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
+        <section className="text-gray-600 body-font main-section mt-5 p-5">
+        <div className="mx-auto row">
+          <div className="col-md-8">
+            <img className="object-cover object-center rounded img-fluid" alt="hero" src={property.photo[0]} />
           </div>
-          <div className="md:text-left items-center text-center md:col-4">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
-              <br className="hidden lg:inline-block" />readymade gluten
+          <div className="col-md-4">
+            <h1 className="title-font sm:text-4xl text-3xl mb-3 font-medium text-gray-900">{property.address}
             </h1>
-            <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+            <div className="d-flex align-items-center">
+            <ApartmentIcon fontSize='large' />
+            <span className="ml-3" style={{marginLeft: "5px", fontSize: "20px"}}>{property.title}</span>
+            </div>
+            <div className="d-flex align-items-center">
+            <DescriptionIcon fontSize='large' />
+            <p className="mb-8 my-2 leading-relaxed" style={{marginLeft: "5px",fontSize: "20px" }}>{property.description}</p>
+            </div>
             <div className="flex justify-center">
               <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
               <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
             </div>
           </div>
         </div>
-      </section>
+</section>
     );
 }
 
