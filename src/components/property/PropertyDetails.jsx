dcd7e8e1090b09032ad6dd0e3b62cb1297eb16fa
MustafaRamadan1 <Mustafa.Ramadan165@gmail.com>
2023-06-29 19:22:27 +0300

    add Buttons and some attributes in propertyDetails page

--- src/components/property/PropertyDetails.jsx
+++ src/components/property/PropertyDetails.jsx
@@ -3,6 +3,10 @@ import { useLocation } from 'react-router-dom';
 import './propertyDetails.css';
 import ApartmentIcon from '@mui/icons-material/Apartment';
 import DescriptionIcon from '@mui/icons-material/Description';
+import LocationCityIcon from '@mui/icons-material/LocationCity';
+import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 const PropertyDetails = () => {
 
     const location = useLocation();
@@ -23,20 +27,46 @@ const PropertyDetails = () => {
           <div className="col-md-4">
             <h1 className="title-font sm:text-4xl text-3xl mb-3 font-medium text-gray-900">{property.address}
             </h1>
-            <div className="d-flex align-items-center">
+            <div className="d-flex align-items-center" style={{marginBottom : "20px"}}>
             <ApartmentIcon fontSize='large' />
-            <span className="ml-3" style={{marginLeft: "5px", fontSize: "20px"}}>{property.title}</span>
+            <span className="ml-3" style={{marginLeft: "5px", fontSize: "20px" }}> {property.title}</span>
             </div>
-            <div className="d-flex align-items-center">
+            <div className="d-flex align-items-center" style={{}}>
             <DescriptionIcon fontSize='large' />
-            <p className="mb-8 my-2 leading-relaxed" style={{marginLeft: "5px",fontSize: "20px" }}>{property.description}</p>
+            <p className="mb-8 my-2 leading-relaxed" style={{marginLeft: "5px",fontSize: "20px",  }}>{property.description}</p>
             </div>
-            <div className="flex justify-center">
-              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
+            <div className="d-flex align-items-center" style={{}}>
+            <LocationCityIcon fontSize='large' />
+            <p className="mb-8 my-2 leading-relaxed" style={{marginLeft: "5px",fontSize: "20px",  }}>{property.city}</p>
             </div>
+            <div className="d-flex align-items-center" style={{}}>
+            <LocalPhoneIcon fontSize='large' />
+            <p className="mb-8 my-2 leading-relaxed" style={{marginLeft: "5px",fontSize: "20px",  }}>{property.contractPhone}</p>
+            </div>
+            <div className="d-flex align-items-center" style={{}}>
+            <AttachMoneyIcon fontSize='large' />
+            <p className="mb-8 my-2 leading-relaxed" style={{marginLeft: "5px",fontSize: "20px",color: "#780000" }}>EGP {property.price}</p>
+            </div>
+            <div>
+      <button
+        className="btn btn-primary btn-lg"
+        style={{ fontSize: '1.5rem', padding: '15px 50px', marginTop: "25px", marginRight: "30px",  borderRadius: "25px" }}
+
+      >
+        <a href={`tel: +201123141912`} style={{ color: 'white', textDecoration: 'none' }}>Call me </a>
+      </button>
+      <button
+        className="btn btn-primary btn-lg"
+        style={{ fontSize: '1.5rem', padding: '15px 50px', marginTop: "25px", borderRadius: "25px" }}
+
+      >
+        <a href={`tel: +201123141912`} style={{ color: 'white', textDecoration: 'none' }}>Whatsapp </a>
+      </button>
+    </div>
+           
           </div>
         </div>
+        
 </section>
     );
 }
