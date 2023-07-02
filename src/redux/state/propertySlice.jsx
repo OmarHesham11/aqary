952c8af2279407d1bf68748c1b767cae57dae034
MontaserHassan <montaser.hassan.bns@gmail.com>
2023-07-03 02:06:45 +0300

    tyr to deploy v.2

--- src/redux/state/propertySlice.jsx
+++ src/redux/state/propertySlice.jsx
@@ -6,111 +6,70 @@ const initialState = {
     error: null,
     searchResults: [],
 };
-// export const createProperty = createAsyncThunk(
-//     "properties/CreateProperty",
-//     async ({ propertyData, image }, thunkAPI) => {
-//         const { rejectWithValue } = thunkAPI;
-//         try {
-//             console.log('hi');
-//             const formData = new FormData();
-//             formData.append("propertyData", JSON.stringify(propertyData));
 
-//             if (image.length > 0) {
-//                 image.forEach((imageFile) => formData.append("image", imageFile));
-//             }
-
-//             const res = await fetch("http://localhost:4000/auth/property/", {
-//                 method: "POST",
-//                 body: formData,
-//                 "Content-Type": "application/json; charset=UTF-8"
-//             });
-
-//             const data = await res.json();
-//             console.log(data);
-//             return data;
-//         } catch (error) {
-//             return rejectWithValue(error.message);
-//         }
-//     }
-// );
-export const createProperty = createAsyncThunk(
-    "properties/CreateProperty",
-    async (formData, thunkAPI) => {
-        const { rejectWithValue } = thunkAPI;
-        console.log(formData.get('image'));
-        console.log(typeof formData);
-        try {
-            axios({
-                method: "post",
-                url: "http://localhost:4000/auth/property/",
-                data: formData,
-                headers: { "Content-Type": "multipart/form-data" },
-            }).then(function (res) {
-                const data = res.data;
-                return data;
-            }).catch(function (response) {
-                console.log(response);
-            });
-            // axios.post(`http://localhost:4000/auth/property/`, formData)
-            // .then(({data}) => {
-            //     return data;
-            // }).catch(function (response) {
-            //     console.log(response);
-            // });
-        } catch (error) {
-            console.log(error);
-            return rejectWithValue(error.message);
-        }
+export const createProperty = createAsyncThunk("properties/CreateProperty", async (formData, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+    console.log(formData.get('image'));
+    console.log(typeof formData);
+    try {
+        axios({
+            method: "post",
+            url: "http://localhost:4000/auth/property/",
+            data: formData,
+            headers: { "Content-Type": "multipart/form-data" },
+        }).then(function (res) {
+            const data = res.data;
+            return data;
+        }).catch(function (response) {
+            console.log(response);
+        });
+        // axios.post(`http://localhost:4000/auth/property/`, formData)
+        // .then(({data}) => {
+        //     return data;
+        // }).catch(function (response) {
+        //     console.log(response);
+        // });
+    } catch (error) {
+        console.log(error);
+        return rejectWithValue(error.message);
     }
+}
 );
 
-
-export const fetchProperties = createAsyncThunk(
-    "properties/fetchProperties",
-    async (currentPage, thunkAPI) => {
-        const { rejectWithValue } = thunkAPI; // to catch error and return it
-        try {
-            const response = await fetch(
-                `https://aqary-eg.onrender.com/property/?page=${currentPage}`
-            );
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            return rejectWithValue(error.message);
-        }
+export const fetchProperties = createAsyncThunk("properties/fetchProperties", async (currentPage, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+    try {
+        const response = await fetch(`https://aqary-eg.onrender.com/property/?page=${currentPage}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return rejectWithValue(error.message);
     }
+}
 );
 
-export const fetchProperty = createAsyncThunk(
-    "properties/fetchProperty",
-    async (propertyId, thunkAPI) => {
-        const { rejectWithValue } = thunkAPI;
-        try {
-            const response = await fetch(
-                `https://aqary-eg.onrender.com/property/${propertyId}`
-            );
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            return rejectWithValue(error.message);
-        }
+export const fetchProperty = createAsyncThunk("properties/fetchProperty", async (propertyId, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+    try {
+        const response = await fetch(`https://aqary-eg.onrender.com/property/${propertyId}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return rejectWithValue(error.message);
     }
+}
 );
 
-export const searchProperties = createAsyncThunk(
-    "properties/searchProperties",
-    async (searchQuery, thunkAPI) => {
-        const { rejectWithValue } = thunkAPI;
-        try {
-            const response = await fetch(
-                `https://aqary-eg.onrender.com/property/search/${searchQuery}`
-            );
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            return rejectWithValue(error.message);
-        }
+export const searchProperties = createAsyncThunk("properties/searchProperties", async (searchQuery, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+    try {
+        const response = await fetch(`https://aqary-eg.onrender.com/property/search/${searchQuery}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return rejectWithValue(error.message);
     }
+}
 );
 
 const propertiesSlice = createSlice({
