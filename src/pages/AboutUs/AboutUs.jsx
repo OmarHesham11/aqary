d7030322560d442a58d147aca6bd1afc87635c37
Amani khaled <Amani.khaled.mohamed@gmail.com>
2023-07-05 07:20:24 +0200

    add emails

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -24,57 +24,57 @@ function AboutUs() {
 
                 <div className="team-content">
                     <div className="box">
-                        <img src={ imag1 } alt="Team Member 1" />
+                        <img src={imag1} alt="Team Member 1" />
                         <h3>Montaser Hassan</h3>
                         <h5>software Engineer</h5>
                         <div className="icons">
-                            <a href="https://www.linkedin.com/in/montaser-hassan/"><FaLinkedin size={ 20 } /></a>
-                            <a href="https://github.com/MontaserHassan"><AiFillGithub size={ 20 } /></a>
-                            <a href="#"><FaEnvelope size={ 20 } /></a>
+                            <a href="https://www.linkedin.com/in/montaser-hassan/"><FaLinkedin size={20} /></a>
+                            <a href="https://github.com/MontaserHassan"><AiFillGithub size={20} /></a>
+                            <a href="montaser.mohamed.hassan@gmail.com"><FaEnvelope size={20} /></a>
                         </div>
                     </div>
 
                     <div className="box">
-                        <img src={ imag2 } alt="Team Member 2" />
+                        <img src={imag2} alt="Team Member 2" />
                         <h3>Omar Hesham</h3>
                         <h5>software Engineer</h5>
                         <div className="icons">
-                            <a href="https://www.linkedin.com/in/omar-hesham-elhariry/"><FaLinkedin size={ 20 } /></a>
-                            <a href="https://github.com/OmarHesham11"><AiFillGithub size={ 20 } /></a>
-                            <a href="#"><FaEnvelope size={ 20 } /></a>
+                            <a href="https://www.linkedin.com/in/omar-hesham-elhariry/"><FaLinkedin size={20} /></a>
+                            <a href="https://github.com/OmarHesham11"><AiFillGithub size={20} /></a>
+                            <a href="#"><FaEnvelope size={20} /></a>
                         </div>
                     </div>
 
                     <div className="box">
-                        <img src={ imag4 } alt="Team Member 3" />
+                        <img src={imag4} alt="Team Member 3" />
                         <h3>Mohamed Adel</h3>
                         <h5>software Engineer</h5>
                         <div className="icons">
-                            <a href="https://www.linkedin.com/in/muhammed-elshall/"><FaLinkedin size={ 20 } /></a>
-                            <a href="https://github.com/mhamdadel"><AiFillGithub size={ 20 } /></a>
-                            <a href="#"><FaEnvelope size={ 20 } /></a>
+                            <a href="https://www.linkedin.com/in/muhammed-elshall/"><FaLinkedin size={20} /></a>
+                            <a href="https://github.com/mhamdadel"><AiFillGithub size={20} /></a>
+                            <a href="#"><FaEnvelope size={20} /></a>
                         </div>
                     </div>
 
                     <div className="box">
-                        <img src={ imag3 } alt="Team Member 4" />
+                        <img src={imag3} alt="Team Member 4" />
                         <h3>Mostafa Ramadan</h3>
                         <h5>software Engineer</h5>
                         <div className="icons">
-                            <a href="https://www.linkedin.com/in/mustafaramadan165/"><FaLinkedin size={ 20 } /></a>
-                            <a href="https://github.com/MustafaRamadan1"><AiFillGithub size={ 20 } /></a>
-                            <a href="#"><FaEnvelope size={ 20 } /></a>
+                            <a href="https://www.linkedin.com/in/mustafaramadan165/"><FaLinkedin size={20} /></a>
+                            <a href="https://github.com/MustafaRamadan1"><AiFillGithub size={20} /></a>
+                            <a href="mustafa.ramadan165@gmail.com"><FaEnvelope size={20} /></a>
                         </div>
                     </div>
 
                     <div className="box">
-                        <img src={ image5 } alt="Team Member 4" />
+                        <img src={image5} alt="Team Member 4" />
                         <h3>Amany khaled</h3>
                         <h5>software Engineer</h5>
                         <div className="icons">
-                            <a href="https://www.linkedin.com/in/amany-khaled-mohamed/"> <FaLinkedin size={ 20 } /></a>
-                            <a href="https://github.com/Amanikhaled"><AiFillGithub size={ 20 } /></a>
-                            <a href="#"><FaEnvelope size={ 20 } /></a>
+                            <a href="https://www.linkedin.com/in/amany-khaled-mohamed/"> <FaLinkedin size={20} /></a>
+                            <a href="https://github.com/Amanikhaled"><AiFillGithub size={20} /></a>
+                            <a href="#"><FaEnvelope size={20} /></a>
                         </div>
                     </div>
                 </div>
